cc77a3c8d54a16a132c5e46ae540921fc084e346
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-09-08 14:24:22 -0700

    express connect

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS candidates;
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger = require('morgan');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(logger('dev'));
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}, http://localhost:${PORT}/`);
+});
