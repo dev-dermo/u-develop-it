212e78f90c895a48443b2a1f792d32d64097f55b
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-09-08 14:30:00 -0700

    query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const logger = require('morgan');
 
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(logger('dev'));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'election'
+}, console.log('Connected to the election database.'));
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
