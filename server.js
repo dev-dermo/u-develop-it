0e113f1913e71ffb7b48454e03884e6896564ffe
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-09-08 14:38:32 -0700

    queries

--- server.js
+++ server.js
@@ -16,8 +16,31 @@ const db = mysql.createConnection({
   database: 'election'
 }, console.log('Connected to the election database.'));
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log(row);
+// });
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
+//   if (err) console.log(err);
+
+//   console.log(results);
+// });
+
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, results) => {
+  if (err) console.log(err);
+
+  console.log(results);
 });
 
 // Default response for any other request (Not Found)
